d793b7a659b729afd597311a069e4c03ca2293e7
Alexis DRAI <drai.alexis@gmail.com>
2021-02-11 22:27:12 +0100

    Add strcmp and strcpy clones

--- main.c
+++ main.c
@@ -4,29 +4,77 @@
 #include <time.h>
 
 int main() {
-    char sentence[] = "Hello, World!";
+    //////////////////////////////////////////////////////////////////////////////
+    /*strlen clone*/
+    char sentenceA[] = "Hallo, World!";
+    char sentenceB[] = "Hello, World!";
     float temps1;
 
     clock_t begin1 = clock();
     for (int i = 0; i < 10000000; ++i) {
-        strlen_y(sentence);
+        strlen_y(sentenceA);
     }
     clock_t end1 = clock();
     temps1 = (float) (end1 - begin1) / CLOCKS_PER_SEC;
 
     printf("strlen_y took %f seconds to iterate 10,000,000 times.\n", temps1);
-////////////////////////////////////////////////////////////////////
-
+    //////////////////////////////////////////////////////////////////////////////
     float temps2;
 
     clock_t begin2 = clock();
     for (int i = 0; i < 10000000; ++i) {
-        strlen(sentence);
+        strlen(sentenceA);
     }
     clock_t end2 = clock();
     temps2 = (float) (end2 - begin2) / CLOCKS_PER_SEC;
 
     printf("strlen took %f seconds to iterate 10,000,000 times.\n", temps2);
+    //////////////////////////////////////////////////////////////////////////////
+    /*strcmp clone*/
+    float temps3;
 
+    clock_t begin3 = clock();
+    for (int i = 0; i < 10000000; ++i) {
+        strcmp_y(sentenceA, sentenceB);
+    }
+    clock_t end3 = clock();
+    temps3 = (float) (end3 - begin3) / CLOCKS_PER_SEC;
+
+    printf("strcmp_y took %f seconds to iterate 10,000,000 times.\n", temps3);
+    //////////////////////////////////////////////////////////////////////////////
+    float temps4;
+    clock_t begin4 = clock();
+    for (int i = 0; i < 10000000; ++i) {
+        strcmp(sentenceA, sentenceB);
+    }
+    clock_t end4 = clock();
+    temps4 = (float) (end4 - begin4) / CLOCKS_PER_SEC;
+
+    printf("strcmp took %f seconds to iterate 10,000,000 times.\n", temps4);
+
+    //////////////////////////////////////////////////////////////////////////////
+    /*strcpy clone*/
+    char sentenceEmpty[] = "what";
+    float temps5;
+
+    clock_t begin5 = clock();
+    for (int i = 0; i < 10000000; ++i) {
+        strcpy_y(sentenceEmpty, sentenceB);
+    }
+    clock_t end5 = clock();
+    temps5 = (float) (end5 - begin5) / CLOCKS_PER_SEC;
+
+    printf("strcpy_y took %f seconds to iterate 10,000,000 times.\n", temps5);
+    //////////////////////////////////////////////////////////////////////////////
+    float temps6;
+
+    clock_t begin6 = clock();
+    for (int i = 0; i < 10000000; ++i) {
+        strcpy(sentenceEmpty, sentenceB);
+    }
+    clock_t end6 = clock();
+    temps6 = (float) (end6 - begin6) / CLOCKS_PER_SEC;
+
+    printf("strcpy took %f seconds to iterate 10,000,000 times.\n", temps6);
     return 0;
 }
--- stringy.h
+++ stringy.h
@@ -5,12 +5,33 @@
 #ifndef TP4BONUS_STRINGY_H
 #define TP4BONUS_STRINGY_H
 
-unsigned long long strlen_y(const char str[]) {
+unsigned long long strlen_y(const char *str) {
     long long i = 0;
     while (str[i] != '\0') {
         ++i;
     }
     return i;
+}
+
+int strcmp_y(const char *lhs, const char *rhs) {
+    int i = 0;
+    while (rhs[i] != '\0' && lhs[i] != '\0') {
+        if (lhs[i] != rhs[i]) {
+            return (lhs[i] - rhs[i]) > 0 ? 1 : -1;
+        }
+        ++i;
+    }
+    return 0;
+}
+
+char *strcpy_y(char *restrict dest, const char *restrict src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        ++i;
+    }
+    dest[i]=src[i]; //copies the '\0'
+    return dest;
 }
 
 #endif //TP4BONUS_STRINGY_H
